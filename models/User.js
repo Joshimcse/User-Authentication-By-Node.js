283c8c107eba886ff7e5503205ed94cf387b218f
acharjeeauntor <acharjeeauntor@gmail.com>
2019-06-30 21:31:09 +0600

    add the validation

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const Sequelize = require("sequelize");
+const sequelize = require("../config/key");
+
+const User = sequelize.define("User", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  name: {
+    type: Sequelize.STRING,
+    required: true
+  },
+  email: {
+    type: Sequelize.STRING,
+    required: true
+  },
+  password: {
+    type: Sequelize.STRING,
+    required: true
+  },
+  date: {
+    type: Sequelize.DATE,
+    deadline: Sequelize.DATE
+  }
+});
+
+module.exports = User;
--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,54 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
 
 router.get("/login", (req, res) => res.render("login"));
 
 router.get("/register", (req, res) => res.render("register"));
 
-// Dashboard
-// router.get("/dashboard", ensureAuthenticated, (req, res) =>
-//   res.render("dashboard", {
-//     user: req.user
-//   })
-// );
+router.post("/register", (req, res) => {
+  //console.log(req.body)
+  const { name, email, password, password2 } = req.body;
+  let errors = [];
+
+  if (!name || !email || !password || !password2) {
+    errors.push({ msg: "please fill all the fields" });
+  }
+
+  if (password !== password2) {
+    errors.push({ msg: "Password do not match" });
+  }
+
+  if (password.length < 6) {
+    errors.push({ msg: "Password should be at least 6 characters" });
+  }
+
+  if (errors.length > 0) {
+    res.render("register", {
+      errors,
+      name,
+      email,
+      password,
+      password2
+    });
+  } else {
+    User.findOne({ email: email })
+      .then((user) => {
+        if (user) {
+          errors.push({msg:'Email Already Registered'})
+          res.render("register", {
+            errors,
+            name,
+            email,
+            password,
+            password2
+          });
+        } else {
+          
+        }
+      })
+      .catch((err) => console.log(err));
+  }
+});
 
 module.exports = router;
